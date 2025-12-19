e05b99acbebbdbad319e62f519419f854d981898
marwan-2004 <162841143+marwan-2004@users.noreply.github.com>
2025-12-19 21:29:47 +0200

    Update 4App.jsx

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,37 +1,35 @@
-
 import './App.css'
-import { createBrowserRouter, HashRouter } from 'react-router-dom'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import About from './components/About/About'
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from './components/Contact/Contact'
 import NotFound from './components/NotFound/NotFound'
 
 
+const REPO_NAME = '1ST_React'  
 
-
-let route=HashRouter([
-  {path:'' , element:<Layout/> , children:[
-  {path:'1ST_React',element:<About/>},
-  {path:'about',element:<About/>},
-  {path:'portfolio',element:<Portfolio/>},
-  {path:'contact',element:<Contact/>},
-  {path:'*',element:<NotFound/>}
-  
-
-  ]},
-  
-]
-
-
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <h1>Home Page</h1> },
+        { path: 'about', element: <About /> },
+        { path: 'portfolio', element: <Portfolio /> },
+        { path: 'contact', element: <Contact /> },
+        { path: '*', element: <NotFound /> }
+      ]
+    }
+  ],
+  {
+    basename: `/${REPO_NAME}`
+  }
 )
-function App() {
-  return <>
 
-  <RouterProvider router={route}/> 
-  
-  </>
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
